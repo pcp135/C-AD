dc38397c9a9c7b5532694d179e944ee7118b0fdc
Phil <phil@parsons.uk.com>
2013-10-04 20:14:19 +0200

    reduced memory version

--- 2HW4/apsp.py
+++ 2HW4/apsp.py
@@ -2,20 +2,23 @@ import sys
 
 def main_loop():
 	for k in range(1,nodes+1):
+		A[k%2]=A[(k-1)%2]
 		for i in range(1,nodes+1):
-			for j in range(1,nodes+1):
-				routes = A.keys()
-				if (i,k,(k-1)%2) in routes and (k,j,(k-1)%2) in routes:
-					if (i,j,(k-1)%2) in routes: 	
-						A[(i,j,k%2)] = min(A[(i,k,(k-1)%2)]+A[(k,j,(k-1)%2)],A[(i,j,(k-1)%2)])
-					else:
-						A[(i,j,k%2)] = A[(i,k,(k-1)%2)]+A[(k,j,(k-1)%2)]
-				elif (i,j,(k-1)%2) in routes: 	
-					A[(i,j,k%2)]=A[(i,j,(k-1)%2)]
-				if i==j and A[(i,j,k%2)]<0:
-					return "Negative cycle detected"
-	return min(A.items(), key=lambda x: x[1])
+			if k in A[(k-1)%2][i].keys():
+				for j in A[(k-1)%2][k].keys():
+					if j not in A[k%2][i].keys() or A[(k-1)%2][i][k]+A[(k-1)%2][k][j]<A[k%2][i][j]:
+						A[k%2][i][j]=A[(k-1)%2][i][k]+A[(k-1)%2][k][j]
+	return A[nodes%2]
 
+def report_min(B):
+	min_path=(0,0)
+	min_length=999
+	for (tail,root) in B.iteritems():
+		for (head,length) in root.iteritems():
+			if length<min_length:
+				min_length=length
+				min_path=(tail,head)
+	return min_path, min_length
 
 input_list=[x.strip().split() for x in file(sys.argv[1],"r")]
 nodes, edges = int(input_list[0][0]), int(input_list[0][1])
@@ -25,7 +28,7 @@ print "Built Graph..."
 print "Nodes: %s, Edges: %s" % (nodes,edges)
 
 #initialize
-A=(dict(),dict())
+A=[dict(),dict()]
 edges = graph.keys()
 for i in range(1,nodes+1):
 	A[0][i]={i:0}
@@ -34,4 +37,4 @@ for (i,j) in edges:
 print "Initialized..."			
 
 #Main Loop
-print main_loop()
+print report_min(main_loop())
